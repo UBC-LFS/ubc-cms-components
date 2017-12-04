7d486f8d06e0f352801e974e2a36198856b249a4
Justin Lee <justinlee0022@gmail.com>
2017-12-04 10:30:55 -0800

    fixed styling

--- filterable-boxes/filterable-boxes.js
+++ filterable-boxes/filterable-boxes.js
@@ -1,17 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  document.getElementById("search").oninput = function () {
-          var matcher = new RegExp(document.getElementById("search").value, "gi");
-          for (var i = 0; i < document.getElementsByClassName("box").length; i++) {
-               if (matcher.test(document.getElementsByClassName("box-name")[i].innerHTML)
-                    || matcher.test(document.getElementsByClassName("details")[i].innerHTML)
-                    || matcher.test(document.getElementsByClassName("tags")[i].innerHTML))
-               {
-                    document.getElementsByClassName("box")[i].style.display =
-                         "inline-block";
-               } else {
-                    document.getElementsByClassName("box")[i].style.display = "none";
-               }
-
-          }
-     }
-});
+document.addEventListener('DOMContentLoaded', function (event) {
+  document.getElementById('search').oninput = function () {
+    var matcher = new RegExp(document.getElementById('search').value, 'gi')
+    for (var i = 0; i < document.getElementsByClassName('box').length; i++) {
+      if (matcher.test(document.getElementsByClassName('box-name')[i].innerHTML) ||
+                    matcher.test(document.getElementsByClassName('details')[i].innerHTML) ||
+                    matcher.test(document.getElementsByClassName('tags')[i].innerHTML)) {
+        document.getElementsByClassName('box')[i].style.display =
+                         'inline-block'
+      } else {
+        document.getElementsByClassName('box')[i].style.display = 'none'
+      }
+    }
+  }
+})
